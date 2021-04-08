99dca3f9186a8edbaba8b76b7e8ee367c954d185
anant-creator <61108547+anant-creator@users.noreply.github.com>
2021-04-08 14:24:58 +0530

    Nested for loop

---
+++ nested-for-loop.js
@@ -0,0 +1,7 @@
+for (let exercise = 1; exercise < 6; exercise++) {
+    console.log(`----- Exercise Set No. ${exercise} -----`);
+
+    for (let lift = 1; lift <= 5; lift++) {
+        console.log(`Exercise ${exercise} -- Dumble Lift no. ${lift} --`);
+    }
+}
